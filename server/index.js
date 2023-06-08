0fdb042c83797c2a63d2946f5bf8b823b1a57361
sourav-malik <85172735+sourav-malik@users.noreply.github.com>
2023-06-08 13:36:00 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,9 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
-
+app.get("/", (req, res) => {
+  res.send("Hello to Memories API");
+});
 
 const PORT = process.env.PORT || 5000;
 
