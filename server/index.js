15f7370814854f5e4f1e278b9448e878c0d66874
sourav-malik <85172735+sourav-malik@users.noreply.github.com>
2023-06-08 13:22:13 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,9 +16,9 @@ app.use(cors());
 app.use('/posts', postRoutes);
 
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
-mongoose.connect("mongodb+srv://Souravmalik123:Singham123@cluster0.4scgebu.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
